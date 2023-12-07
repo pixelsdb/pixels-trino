df04bbf4785c99de2aefdff5bf939432b6b86945
Tiannan Sha <tiannansha@gmail.com>
2023-12-07 13:14:18 +0100

    implement three types of distances: euclidean, dotproduct and cosine similarity

--- connector/src/main/java/io/pixelsdb/pixels/trino/vector/VectorUDF.java
+++ connector/src/main/java/io/pixelsdb/pixels/trino/vector/VectorUDF.java
@@ -25,7 +25,6 @@ public class VectorUDF {
             @SqlNullable @SqlType("array(double)") Block vector)
     {
         // for now just retreive each element of the vector
-        //todo deploy and set up debbug environment
 
         ArrayList<Double> features = new ArrayList<>();
 
@@ -40,5 +39,88 @@ public class VectorUDF {
             sum += v;
         }
         return sum;
+    }
+
+    @ScalarFunction("eucDist")
+    @Description("calculate the Euclidean distance between two vectors")
+    @SqlType(StandardTypes.DOUBLE)
+    @SqlNullable
+    public static Double eucDist(
+            @SqlNullable @SqlType("array(double)") Block vec1,
+            @SqlNullable @SqlType("array(double)") Block vec2)
+    {
+        if (!distIsDefined(vec1, vec2)) {
+            return null;
+        }
+
+        double dist = 0.0;
+        for (int position = 0; position < vec1.getPositionCount(); position++) {
+            //todo can also use multi threads and let different threads be responsible for different elements
+            // one thread for calculating (x[1]-y[1])^2, another (x[2]-y[2))^2
+            // let's keep it simple and only use single thread for now
+            double xi = DOUBLE.getDouble(vec1, position);
+            double yi = DOUBLE.getDouble(vec2, position);
+            dist += (xi-yi)*(xi-yi);
+        }
+        return dist;
+    }
+
+    @ScalarFunction("dotProd")
+    @Description("calculate the dot product between two vectors")
+    @SqlType(StandardTypes.DOUBLE)
+    @SqlNullable
+    public static Double dotProd(
+            @SqlNullable @SqlType("array(double)") Block vec1,
+            @SqlNullable @SqlType("array(double)") Block vec2)
+    {
+        if (!distIsDefined(vec1, vec2)) {
+            return null;
+        }
+
+        double dist = 0.0;
+        for (int position = 0; position < vec1.getPositionCount(); position++) {
+            //todo can also use multi threads and let different threads be responsible for different elements
+            // one thread for calculating x[1]*y[1], another x[2]*y[2]
+            // let's keep it simple and only use single thread for now
+            double xi = DOUBLE.getDouble(vec1, position);
+            double yi = DOUBLE.getDouble(vec2, position);
+            dist += xi*yi;
+        }
+        return dist;
+    }
+
+    @ScalarFunction("cosSim")
+    @Description("calculate the cosine similarity between two vectors")
+    @SqlType(StandardTypes.DOUBLE)
+    @SqlNullable
+    public static Double cosSim(
+            @SqlNullable @SqlType("array(double)") Block vec1,
+            @SqlNullable @SqlType("array(double)") Block vec2)
+    {
+        if (!distIsDefined(vec1, vec2)) {
+            return null;
+        }
+
+        double dotProd = 0.0;
+        double vec1L2Norm = 0.0;
+        double vec2L2Norm = 0.0;
+        for (int position = 0; position < vec1.getPositionCount(); position++) {
+            //todo can also use multi threads and let different threads be responsible for different elements
+            // one thread for calculating x[1]*y[1], another x[2]*y[2]
+            // let's keep it simple and only use single thread for now
+            double xi = DOUBLE.getDouble(vec1, position);
+            double yi = DOUBLE.getDouble(vec2, position);
+            dotProd += xi*yi;
+            vec1L2Norm += xi*xi;
+            vec2L2Norm += yi*yi;
+        }
+        return dotProd / (Math.sqrt(vec1L2Norm) * Math.sqrt(vec2L2Norm));
+    }
+
+    private static boolean distIsDefined(Block vec1, Block vec2) {
+        if (vec1!=null && vec2!=null && vec1.getPositionCount()==vec2.getPositionCount()) {
+            return true;
+        }
+        return false;
     }
 }
