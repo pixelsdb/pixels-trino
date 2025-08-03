60b38cc0fcd6ee044f50b7b0a2389c02d9944656
antio2 <antio2@qq.com>
2025-08-03 14:24:07 +0800

    cache update

--- connector/src/main/java/io/pixelsdb/pixels/trino/PixelsPageSourceProvider.java
+++ connector/src/main/java/io/pixelsdb/pixels/trino/PixelsPageSourceProvider.java
@@ -49,8 +49,8 @@ public class PixelsPageSourceProvider implements ConnectorPageSourceProvider
     private static final Logger logger = Logger.get(PixelsPageSourceProvider.class);
 
     private final String connectorId;
-    private final MemoryMappedFile cacheFile;
-    private final MemoryMappedFile indexFile;
+    private final List<MemoryMappedFile> cacheFiles;
+    private final List<MemoryMappedFile> indexFiles;
     private final PixelsFooterCache pixelsFooterCache;
     private final PixelsTrinoConfig config;
 
@@ -63,16 +63,24 @@ public class PixelsPageSourceProvider implements ConnectorPageSourceProvider
         if (config.getConfigFactory().getProperty("cache.enabled").equalsIgnoreCase("true"))
         {
             // NOTICE: creating a MemoryMappedFile is efficient, usually cost tens of us.
-            this.cacheFile = new MemoryMappedFile(
-                    config.getConfigFactory().getProperty("cache.location"),
-                    Long.parseLong(config.getConfigFactory().getProperty("cache.size")));
-            this.indexFile = new MemoryMappedFile(
-                    config.getConfigFactory().getProperty("index.location"),
-                    Long.parseLong(config.getConfigFactory().getProperty("index.size")));
+            int zoneNum = Integer.parseInt(config.getConfigFactory().getProperty("cache.zone.num"));
+            int swapZoneNum = Integer.parseInt(config.getConfigFactory().getProperty("cache.zone.swap.num"));
+            long zoneSize = Long.parseLong(config.getConfigFactory().getProperty("cache.size")) / (zoneNum - swapZoneNum);
+            long zoneIndexSize = Long.parseLong(config.getConfigFactory().getProperty("index.size")) / (zoneNum - swapZoneNum);
+            String zoneLocationPrefix = config.getConfigFactory().getProperty("cache.location");
+            String indexLocationPrefix = config.getConfigFactory().getProperty("index.location");
+            this.cacheFiles = new java.util.ArrayList<>();
+            this.indexFiles = new java.util.ArrayList<>();
+            for (int i = 0; i < zoneNum; i++)
+            {
+                this.cacheFiles.add(new MemoryMappedFile(zoneLocationPrefix + "." + i, zoneSize));
+                this.indexFiles.add(new MemoryMappedFile(indexLocationPrefix + "." + i, zoneIndexSize));
+            }
+            this.indexFiles.add(new MemoryMappedFile(indexLocationPrefix, zoneIndexSize));
         } else
         {
-            this.cacheFile = null;
-            this.indexFile = null;
+            this.cacheFiles = new java.util.ArrayList<>();
+            this.indexFiles = new java.util.ArrayList<>();
         }
         this.pixelsFooterCache = new PixelsFooterCache();
     }
@@ -96,18 +104,18 @@ public class PixelsPageSourceProvider implements ConnectorPageSourceProvider
             Storage storage = StorageFactory.Instance().getStorage(pixelsSplit.getStorageScheme());
             if(pixelsSplit instanceof PixelsFileSplit) {
                 PixelsFileSplit pixelsFileSplit = (PixelsFileSplit) pixelsSplit;
-                
+
                 if (pixelsFileSplit.getFromServerlessOutput())
                 {
                     IntermediateFileCleaner.Instance().registerStorage(storage);
                     return new PixelsFilePageSource(pixelsFileSplit, pixelsColumns, pixelsTransactionHandle, storage,
-                            cacheFile, indexFile, pixelsFooterCache);
+                            cacheFiles, indexFiles, pixelsFooterCache);
                 } else
                 {
                     // perform scan push down.
                     List<PixelsColumnHandle> withFilterColumns = getIncludeColumns(pixelsColumns, tableHandle);
                     return new PixelsFilePageSource(pixelsFileSplit, withFilterColumns, pixelsTransactionHandle, storage,
-                            cacheFile, indexFile, pixelsFooterCache);
+                            cacheFiles, indexFiles, pixelsFooterCache);
                 }
             }
 
